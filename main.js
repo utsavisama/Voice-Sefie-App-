ff877c4c5ba34a2dbf8833a4f8d7c5df79026199
utsavisama <82657781+utsavisama@users.noreply.github.com>
2021-08-20 17:57:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("text_box").innerHTML = "";
+    recognition.start()
+}
+
+recognition.onresult = function (event) {
+    console.log(event)
+    var person_text = event.results[0][0].transcript;
+
+    document.getElementById("text_box").innerHTML = person_text;
+    console.log(person_text);
+
+    if(person_text == "Google take my selfie") {
+        console.log("taking selfie -------");
+        speak();
+    }
+}
+
+
+function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera)
+
+
+
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+
+    }, 5000)
+
+    
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+})
+
+
+camera = document.getElementById("camera")
+
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "selfie_image" src = "' +data_uri+'"/>'
+    })
+}
+
+function save(){
+    link = document.getElementById("link")
+    image = document.getElementById("selfie_image").src;
+    link.href = image
+    link.click()
+}
